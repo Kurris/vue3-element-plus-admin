309212ea50fae9e98a62153b0b9c1c185f1a1bb7
Ligy <Ligy.97@foxmail.com>
2022-04-23 23:33:44 +0800

    add 确认和取消的小组件

---
+++ src/components/ui/ConfirmOrCancel.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="coc">
+        <el-icon color="#04699d" :size="20" @click="$emit('confirm')">
+            <circle-check />
+        </el-icon>
+        <el-icon color="#ffc833" :size="20" @click="$emit('cancel')">
+            <circle-close />
+        </el-icon>
+    </div>
+</template>
+ <script lang="ts">
+export default {
+    name: ''
+}
+</script>
+<script setup lang="ts">
+
+defineEmits<{
+    (e: 'confirm'): void
+    (e: 'cancel'): void
+}>()
+
+</script>
+<style scoped lang="scss">
+.coc {
+    display: flex;
+}
+
+.el-icon {
+    margin-left: 5px;
+}
+
+
+.el-icon {
+    :hover {
+        cursor: pointer;
+    }
+}
+</style>
